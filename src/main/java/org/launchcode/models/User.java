5611686aacc8704b843878d5e8b3cb4659cc5eb1
MrsKing2023 <smarshall84@yahoo.com>
2023-10-09 21:25:28 -0400

    added validation notations

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,19 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(max = 6)
     private String password;
 
     public User() {
@@ -15,6 +26,11 @@ public class User {
         this.email = email;
         this.password = password;
     }
+    //TODO: add validation notations for username and password,and verify are required (they can’t be empty)
+    //TODO: Username is between 5 and 15 characters
+    //TODO: Email is optional
+    //TODO: If provided, the email has the format of a valid email address.
+    //TODO: The password is at least 6 characters long
 
     public String getUsername() {
         return username;
